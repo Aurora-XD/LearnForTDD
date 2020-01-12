f15fdb590a543ef6b460914f84a7b04db3eb011e
yangchen <chen.yang1@thoughtworks.com>
2020-01-12 15:47:42 +0800

    add function to get FizzBuzz when number could divisible by 3 or 5 or 7

--- src/main/java/FizzBuzzWhizz.java
+++ src/main/java/FizzBuzzWhizz.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class FizzBuzzWhizz {
+    private final String fizz = "Fizz";
+    private final String buzz = "Buzz";
+    private final String whizz = "Whizz";
+
+    public String fizzBuzz(int number) {
+
+        return getResultForDivision(number);
+    }
+
+    private String getResultForDivision(int number){
+        if(number%3 == 0){
+            return fizz;
+        }
+        if(number%5 == 0){
+            return buzz;
+        }
+        if(number%7 == 0){
+            return whizz;
+        }
+        return String.valueOf(number);
+    }
+
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class FizzBuzzTest {
+    private FizzBuzzWhizz fizzBuzzWhizz;
+
+    @Before
+    public void setUp(){
+        fizzBuzzWhizz = new FizzBuzzWhizz();
+    }
+
+    @Test
+    public void should_return_origin_number_when_could_not_divisible_by_3_or_5_or_7() {
+        String result = fizzBuzzWhizz.fizzBuzz(1);
+        Assert.assertEquals("1",result);
+    }
+
+    @Test
+    public void should_return_Fizz_when_number_could_divisible_by_3() {
+        String result = fizzBuzzWhizz.fizzBuzz(3);
+        String expect = "Fizz";
+        Assert.assertEquals(expect,result);
+    }
+
+    @Test
+    public void should_return_Buzz_when_number_could_divisible_by_5() {
+        String result = fizzBuzzWhizz.fizzBuzz(5);
+        String expect = "Buzz";
+        Assert.assertEquals(expect,result);
+    }
+
+    @Test
+    public void should_return_Buzz_when_number_could_divisible_by_7() {
+        String result = fizzBuzzWhizz.fizzBuzz(7);
+        String expect = "Whizz";
+        Assert.assertEquals(expect,result);
+    }
 }
